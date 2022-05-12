0e49046a1d2f89c948462b5dcffc2b9056c4e8e6
suganeshflavio <eshwarsugan5@gmail.com>
2022-05-12 17:52:33 +0530

    1stcommit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import instance from './AxiosInstance';
+
+const token=localStorage.getItem('tok');
+if(token)
+instance.defaults.headers.common['adminauthtoken']=token;
+console.log(token);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
